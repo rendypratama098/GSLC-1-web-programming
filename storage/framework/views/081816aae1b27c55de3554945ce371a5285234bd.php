ee3101caf2b30d6848c8d8d4caa92e40832e824e
rendypratama098 <117034297+rendypratama098@users.noreply.github.com>
2022-11-03 23:37:30 +0700

    Add files via upload

---
+++ storage/framework/views/081816aae1b27c55de3554945ce371a5285234bd.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo $__env->yieldContent('title'); ?></title>
+</head>
+<body>
+    <div>
+        <h1>Home</h1>
+    </div>
+    <div>
+        <?php echo $__env->yieldContent('content'); ?>
+    </div>
+</body>
+</html>
+<?php /**PATH D:\Laravel\GSLC_1_Web_Programming\resources\views/template.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/4d469921df9273294e005d514a3f7498b9a6361a.php
@@ -0,0 +1,20 @@
+<?php $__env->startSection('title', 'Halaman Utama'); ?>
+<?php $__env->startSection('content'); ?>
+<div>
+    <h1>Tenis Meja</h1>
+</div>
+<h2>A. Pengertian Tenis Meja</h2>
+<h3>Tenis meja merupakan salah satu cabang olahraga yang dimainkan oleh dua orang pemain secara tunggal atau dua pasang pemain secara ganda dengan cara berhadapan dengan lawan memakai sebuah bola kecil, bet yang terbuat dari bahan kayu yang dilapisi menggunakan karet, dan lapangan tenis berupa meja. Induk organisasi dalam olahraga tenis meja atau ping pong ini yaitu ITTF atau International Table Tennis Federation bagi tingkat dunia, dan PTMSI atau Persatuan Tenis Meja Seluruh Indonesia bagi tingkat nasional.</h3>
+<br>
+<br>
+<h2>B. Sejarah Tenis Meja</h2>
+<h3>Permainan tenis meja dikenal oleh masyarakat Inggris sejak abad ke 19. Saat itu, tenis meja dikenal dengan nama ping pong, whiff whaff atau gossima. Salah satu nama yang digunakan bagi seluruh negara yaitu ping pong. Bahkan ada sebuah perusahaan dari Inggris yakni J. Jaques and Son, Ltd., menjadikannya sebagai merek dagangnya pada tahun 1901.<br>
+    Semenjak itulah, ping pong hanya dipakai bila permainan itu memakai peralatan dari Jacques, sementara perusahaan lainnya memberikan namanya yaitu table tennis. Pada masa itu, tenis meja menjadi sebuah permainan di kalangan kelas atas, dan kerap dimainkan di dalam ruangan sesudah makan malam.<br>
+    Pada awalnya, peralatan yang dipakai terdiri dari sebuah meja, sebaris buku yang telah disusun diletakan di bagian tengah meja yang digunakan sebagai net, bola golf, dan juga dua buah buku sebagai pemukul bola tersebut. Akan tetapi, di tahun 1901, seorang pecinta tenis meja bernama James W. Gibb berhasil menemukan sebuah bola seluloid.<br>
+    Di tahun yang sama pula, seorang pecinta tenis meja bernama E.C. Goode membuat suatu bet dengan versi modern yakni dengan cara memasang selembar lapisan karet yang terdapat bintik-bintik pada papan kayu dengan permukaannya dihaluskan.<br>
+    Negara Inggris mendirikan Table Tennis Association atau biasa dikenal dengan istilah TTA di tahun 1921, serta diikuti berdirinya organisasi tenis meja bernama International Table Tennis Federation atau ITTF di tahun 1926, dan USA Table Tennis atau USAR di tahun 1943. Kejuaraan tenis meja ini pertama kali dilangsungkan di negara Inggris tepatnya berada di London tahun 1926.<br>
+    Di tahun 1950, sebuah perusahaan alat olahraga yang berada di Inggris bernama S.W. Hancock, Ltd., memperkenalkan produk bet baru dengan bahan kayu berlapiskan karet yang kemudian disatukan dengan lapisan spons pada bagian dasarnya sehingga dapat mengakibatkan tingkat kecepatan, dan perputaran pada bola berubah menjadi tinggi.<br>
+    Di negara Indonesia sendiri, olahraga tenis meja pertama kali diperlombakan pada sebuah ajang Pekan Olahraga Nasional atau biasa disebut dengan nama PON yang diadakan di Solo tahun 1948. Sementara itu, pada tingkat dunia, olahraga ini resmi dipertandingkan pada olimpiade yang diadakan di Seoul, Korea Selatan tahun 1988.<br><br><font color="red">Sumber Informasi: https://www.gramedia.com/literasi/tenis-meja/</font></h3>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Laravel\GSLC_1_Web_Programming\resources\views/home.blade.php ENDPATH**/ ?>
